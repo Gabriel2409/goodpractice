359aa57322cead0cf6089701abeb350591714a10
Gabriel2409 <gabriel.cournelle@gmail.com>
2021-09-19 18:23:39 +0200

    black

--- README.md
+++ README.md
@@ -1,14 +1,31 @@
 # Good practices for a new repo
 
 ## Virtual environment
+
 - `python3 -m venv venv`
 - `source venv/bin/activate`
 - `pip install --upgrade pip`
 
-## Requirementts
+## Requirements
+
 - Create a requirements.txt file and add the necessary packages in it
+- To install requirements : `pip install -r requirements.txt`
 
+## Requirements dev
 
+### black : code formatter
+
+- command line : `black <filename>`
+- in vscode, when trying to format for the first time, choose black to format
+  code
+- Bonus : format on save
+
+in your settings.json
+
+```json
+"python.formatting.provider": "black",
+"editor.formatOnSave": true
+```
 
 # Creation of package (src folder)
 
--- string_count.py
+++ string_count.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 
+
 def count_a_in_string(mystring):
     return Counter(mystring)["a"]
+
 
 def convert_df_to_unique_string(df):
     return df.astype(str).sum().sum()
@@ -16,7 +18,12 @@ def main():
     nb_a = count_a_in_string(mystr)
     print(f"There are {nb_a} 'a' in the string '{mystr}'")
 
-    my_df = pd.DataFrame({"col1":['The lazy', 'airplane'], 'col2':["is in the airport", "of San Francisco"]})
+    my_df = pd.DataFrame(
+        {
+            "col1": ["The lazy", "airplane"],
+            "col2": ["is in the airport", "of San Francisco"],
+        }
+    )
     print("My dataframe")
     print(my_df)
     combined_str = convert_df_to_unique_string(my_df)
@@ -24,11 +31,5 @@ def main():
     print("Total nb of a in df:", count_a_in_string(combined_str))
 
 
-if __name__== '__main__'  :
+if __name__ == "__main__":
     main()
-
-
-
-
-
-
